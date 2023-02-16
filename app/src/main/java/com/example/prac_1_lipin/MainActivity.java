aba855e140a4383dc33873fd0e2c6bde58ec9917
efiml <efimlipin@yandex.ru>
2023-02-16 21:32:08 +0300

    Implemented Toast in all lifecycle methods

--- app/src/main/java/com/example/prac_1_lipin/MainActivity.java
+++ app/src/main/java/com/example/prac_1_lipin/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.prac_1_lipin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,31 +11,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        useToast("onCreate is called");
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+        useToast("onStart is called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        useToast("onStop is called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        useToast("onDestroy is called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        useToast("onPause is called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        useToast("onResume is called");
+    }
+
+    public void useToast(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
 }
